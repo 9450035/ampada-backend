6ee84262dfc2469b23191adb9bacddf2254819c1
amir <amirhali2022@gmail.com>
2022-10-11 01:16:33 +0330

    inisial-commit

---
+++ src/main/java/com/ampadabackend/tms/AmpadaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ampadabackend.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpadaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmpadaBackendApplication.class, args);
+    }
+
+}
