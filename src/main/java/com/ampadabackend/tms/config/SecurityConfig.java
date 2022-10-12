e27fde9f10f73bc3716d8a69b8ae9ad30d6c707c
amir <amirhali2022@gmail.com>
2022-10-13 00:20:32 +0330

    add swagger

--- src/main/java/com/ampadabackend/tms/config/SecurityConfig.java
+++ src/main/java/com/ampadabackend/tms/config/SecurityConfig.java
@@ -2,6 +2,11 @@ package com.ampadabackend.tms.config;
 
 import com.ampadabackend.tms.security.jwt.AuthEntryPointJwt;
 import com.ampadabackend.tms.security.jwt.AuthTokenFilter;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
